d7295aff25ab7ce575592c316a11b84ebf1ba484
Rishita Gusain <rishitgusain21@gmail.com>
2023-09-10 13:53:39 +0530

    files added

---
+++ pages/index.js
@@ -0,0 +1,257 @@
+import styles from "./index.module.css";
+const LandingPageDesktop = () => {
+  return (
+    <div className={styles.landingPageDesktop}>
+      <div className={styles.landingPageDesktopChild} />
+      <img
+        className={styles.screenshot6691Icon}
+        alt=""
+        src="/screenshot-669-1@2x.png"
+      />
+      <img
+        className={styles.landingPageDesktopItem}
+        alt=""
+        src="/rectangle-404.svg"
+      />
+      <div className={styles.landingPageDesktopInner} />
+      <img
+        className={styles.rectangleIcon}
+        alt=""
+        src="/rectangle-400@2x.png"
+      />
+      <div className={styles.frameParent}>
+        <div className={styles.frameGroup}>
+          <div className={styles.contactUsParent}>
+            <div className={styles.contactUs}>Contact Us</div>
+            <div className={styles.loremIpsumPvtLtd51MagalParent}>
+              <div className={styles.loremIpsumPvt}>
+                Lorem Ipsum Pvt Ltd.5/1, Magalton Road, Phartosh Gate near YTM
+                Market, XYZ-343434
+              </div>
+              <div className={styles.example2020gmailcom}>
+                example2020@gmail.com
+              </div>
+              <div className={styles.div}>(904) 443-0343</div>
+            </div>
+          </div>
+          <div className={styles.moreParent}>
+            <div className={styles.more}>More</div>
+            <div className={styles.socialLinks}>Social Links</div>
+            <div className={styles.productsParent}>
+              <div className={styles.products}>Products</div>
+              <div className={styles.career}>Career</div>
+              <div className={styles.contactUs1}>Contact Us</div>
+              <div className={styles.aboutUs}>About Us</div>
+            </div>
+          </div>
+          <div className={styles.foodTruckExample}>
+            © 2022 Food Truck Example
+          </div>
+        </div>
+        <img className={styles.componentIcon} alt="" src="/component.svg" />
+        <img className={styles.componentIcon1} alt="" src="/component1.svg" />
+        <img className={styles.path3Icon} alt="" src="/path-3.svg" />
+      </div>
+      <div className={styles.exploreNow}>
+        <b className={styles.exploreNow1}>Explore Now!</b>
+      </div>
+      <b className={styles.discoverTheBestContainer}>
+        <p className={styles.discoverThe}>{`Discover the `}</p>
+        <p className={styles.discoverThe}>
+          <span className={styles.best}>Best</span>
+          <span> Food and Drinks</span>
+        </p>
+      </b>
+      <div
+        className={styles.naturallyMadeHealthcare}
+      >{`Naturally made Healthcare Products for the better care & support of your body.`}</div>
+      <img className={styles.aboutgirlLpIcon} alt="" src="/aboutgirl-lp.svg" />
+      <div className={styles.rectangleDiv} />
+      <img
+        className={styles.screenshot6692Icon}
+        alt=""
+        src="/screenshot-669-2@2x.png"
+      />
+      <img className={styles.vectorIcon} alt="" src="/vector-1.svg" />
+      <div className={styles.getInTouchWrapper}>
+        <div className={styles.getInTouch}>Get In Touch</div>
+      </div>
+      <img
+        className={styles.landingPageDesktopChild1}
+        alt=""
+        src="/vector-2.svg"
+      />
+      <div className={styles.latestArticlesParent}>
+        <div className={styles.latestArticles}>Latest Articles</div>
+        <div className={styles.grilledTomatoes1846x8461Parent}>
+          <img
+            className={styles.grilledTomatoes1846x8461Icon}
+            alt=""
+            src="/grilledtomatoes1846x846-1@2x.png"
+          />
+          <div className={styles.frameContainer}>
+            <div className={styles.grilledTomatoesAtHomeParent}>
+              <b className={styles.grilledTomatoesAt}>
+                Grilled Tomatoes at Home
+              </b>
+              <div className={styles.ploremIpsumIs}>
+                PLorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's
+                standard...
+              </div>
+            </div>
+            <div className={styles.readMoreWrapper}>
+              <div className={styles.readMore}>Read More</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.frameDiv}>
+          <div className={styles.frameWrapper}>
+            <div className={styles.frameParent1}>
+              <div className={styles.snacksForTravelParent}>
+                <b className={styles.grilledTomatoesAt}>Snacks for Travel</b>
+                <div className={styles.ploremIpsumIs}>
+                  PLorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard...
+                </div>
+              </div>
+              <div className={styles.readMoreWrapper}>
+                <div className={styles.readMore}>Read More</div>
+              </div>
+            </div>
+          </div>
+          <img
+            className={styles.mealPrepIdeas846x8461Icon}
+            alt=""
+            src="/mealprepideas846x846-1@2x.png"
+          />
+        </div>
+        <div className={styles.frameParent2}>
+          <div className={styles.frameWrapper}>
+            <div className={styles.frameParent1}>
+              <div className={styles.snacksForTravelParent}>
+                <b className={styles.grilledTomatoesAt}>Post-workout Recipes</b>
+                <div className={styles.ploremIpsumIs}>
+                  PLorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard...
+                </div>
+              </div>
+              <div className={styles.readMoreFrame}>
+                <div className={styles.readMore}>Read More</div>
+              </div>
+            </div>
+          </div>
+          <img
+            className={styles.mealPrepIdeas846x8461Icon}
+            alt=""
+            src="/mealprepideas846x846-11@2x.png"
+          />
+        </div>
+      </div>
+      <div className={styles.frameParent4}>
+        <div className={styles.frameWrapper2}>
+          <div className={styles.aboutUsParent}>
+            <div className={styles.aboutUs1}>About Us</div>
+            <div className={styles.loremIpsumIs}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. t has survived
+              not only five centuries.
+            </div>
+          </div>
+        </div>
+        <div className={styles.groupDiv}>
+          <div className={styles.readMore3}>Read More</div>
+        </div>
+      </div>
+      <div className={styles.div1}>1/2</div>
+      <img className={styles.frameIcon} alt="" src="/frame-322.svg" />
+      <img className={styles.groupIcon} alt="" src="/group-323.svg" />
+      <div className={styles.landingPageDesktopChild2} />
+      <div className={styles.div2}>2/2</div>
+      
+      <img
+        className={styles.landingPageDesktopChild3}
+        alt=""
+        src="/frame-324.svg"
+      />
+      <img
+        className={styles.landingPageDesktopChild4}
+        alt=""
+        src="/group-325.svg"
+      />
+      <div className={styles.latestArticlesGroup}>
+        <div className={styles.latestArticles}>Latest Articles</div>
+        <div className={styles.grilledTomatoes1846x8461Parent}>
+          <img
+            className={styles.grilledTomatoes1846x8461Icon}
+            alt=""
+            src="/grilledtomatoes1846x846-11@2x.png"
+          />
+          <div className={styles.frameContainer}>
+            <div className={styles.grilledTomatoesAtHomeParent}>
+              <b className={styles.grilledTomatoesAt}>How To Grill Corn</b>
+              <div className={styles.ploremIpsumIs}>
+                PLorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry's
+                standard...
+              </div>
+            </div>
+            <div className={styles.readMoreWrapper}>
+              <div className={styles.readMore}>Read More</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.frameDiv}>
+          <div className={styles.frameWrapper}>
+            <div className={styles.frameParent1}>
+              <div className={styles.snacksForTravelParent}>
+                <b className={styles.grilledTomatoesAt}>Crunchwrap Supreme</b>
+                <div className={styles.ploremIpsumIs}>
+                  PLorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard...
+                </div>
+              </div>
+              <div className={styles.readMoreWrapper}>
+                <div className={styles.readMore}>Read More</div>
+              </div>
+            </div>
+          </div>
+          <img
+            className={styles.mealPrepIdeas846x8461Icon}
+            alt=""
+            src="/mealprepideas846x846-12@2x.png"
+          />
+        </div>
+        <div className={styles.frameParent2}>
+          <div className={styles.frameWrapper}>
+            <div className={styles.frameParent1}>
+              <div className={styles.snacksForTravelParent}>
+                <b className={styles.grilledTomatoesAt}>Broccoli Cheese Soup</b>
+                <div className={styles.ploremIpsumIs}>
+                  PLorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard...
+                </div>
+              </div>
+              <div className={styles.readMoreFrame}>
+                <div className={styles.readMore}>Read More</div>
+              </div>
+            </div>
+          </div>
+          <img
+            className={styles.mealPrepIdeas846x8461Icon}
+            alt=""
+            src="/mealprepideas846x846-13@2x.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPageDesktop;
